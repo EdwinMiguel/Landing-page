ed7ef0d1bc96db0dc710736773cb23ea242308b1
Edwin Orellana <miguelorellanasotomayor@gmail.com>
2022-09-06 16:05:53 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -20,16 +20,17 @@ async function fetchData(urlApi) {
     const artists = await fetchData(API);
     let view = `
     ${artists.recently_played_artists.map(artist => `
+      
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
           <img src="${artist.image_url}" alt="${artist.name}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
+        <a href="${artist.uri}" target="_blank"><h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"></span>
             ${artist.name}
-          </h3>
+          </h3></a>
         </div>
       </div>
     `).slice(0,5).join('')}
